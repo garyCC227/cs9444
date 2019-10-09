f58a2890b3e3f0e14a9390c2186215db955f07a9
garyCC227 <z5163479@ad.unsw.edu.au>
2019-10-09 21:29:14 +1100

    done some of part1.py

--- hw1/src/part1.py
+++ hw1/src/part1.py
@@ -19,6 +19,7 @@ def simple_addition(x, y):
     """
     TODO: Implement a simple addition function that accepts two tensors and returns the result.
     """
+    return x + y
 
 
 # Resize tensors
@@ -28,13 +29,21 @@ def simple_reshape(x, shape):
     """
     TODO: Implement a function that reshapes the given tensor as the given shape and returns the result.
     """
+    return x.view(shape)
 
 
 def simple_flat(x):
     """
     TODO: Implement a function that flattens the given tensor and returns the result.
     """
-
+    flatten_size = 0;
+    for which_layer, size in enumerate(x.shape):
+        if which_layer == 0:
+            flatten_size = size;
+        else:
+            flatten_size *= size
+    
+    return flatten_size
 
 # Transpose and Permutation
 
@@ -43,6 +52,7 @@ def simple_transpose(x):
     TODO: Implement a function that swaps the first dimension and
         the second dimension of the given matrix x and returns the result.
     """
+    return x.transpose(0,1)
 
 
 def simple_permute(x, order):
@@ -50,7 +60,7 @@ def simple_permute(x, order):
     TODO: Implement a function that permute the dimensions of the given tensor
         x according to the given order and returns the result.
     """
-
+    return x.permute(order)
 
 # Matrix multiplication (with broadcasting).
 
@@ -59,6 +69,7 @@ def simple_dot_product(x, y):
     TODO: Implement a function that computes the dot product of
         two rank 1 tensors and returns the result.
     """
+    return x * y
 
 
 def simple_matrix_mul(x, y):
@@ -66,6 +77,7 @@ def simple_matrix_mul(x, y):
     TODO: Implement a function that performs a matrix multiplication
         of two given rank 2 tensors and returns the result.
     """
+    return torch.mm(x,y)
 
 
 def broadcastable_matrix_mul(x, y):
@@ -73,6 +85,7 @@ def broadcastable_matrix_mul(x, y):
     TODO: Implement a function that computes the matrix product of two tensors and returns the result.
         The function needs to be broadcastable.
     """
+    return None
 
 
 # Concatenate and stack.
@@ -81,6 +94,8 @@ def simple_concatenate(tensors):
     TODO: Implement a function that concatenates the given sequence of tensors
         in the first dimension and returns the result
     """
+    result = torch.cat(tensors,0)
+    return result
 
 
 def simple_stack(tensors, dim):
@@ -88,5 +103,6 @@ def simple_stack(tensors, dim):
     TODO: Implement a function that concatenates the given sequence of tensors
         along a new dimension(dim) and returns the result.
     """
+    return torch.cat(tensors,dim)
 
 
